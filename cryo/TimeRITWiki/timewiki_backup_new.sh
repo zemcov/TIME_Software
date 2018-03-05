4f19c03ae4df3a768f153516d639b31122a85ca5
pilot1 <pilot1@time.rit.edu>
2018-03-05 10:32:23 -0500

    wiki backup updates

--- cryo/TimeRITWiki/timewiki_backup_new.sh
+++ cryo/TimeRITWiki/timewiki_backup_new.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-/usr/bin/rsync -a --no-links --delete /usr/local/share/moin /media/pilot1/external/ >> /tmp/timewiki_backup.log 2>&1 /
+/usr/bin/rsync /usr/local/share/moin /media/pilot1/external/ >> /tmp/timewiki_backup.log 2>&1 /
 cat /tmp/timewiki_backup.log >> /var/log/crond/timewiki_backup.log /
 rm /tmp/timewiki_backup.log /
