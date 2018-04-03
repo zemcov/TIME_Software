89cf86cdd4c1bd407bcd02c606366133a92f8b38
ausderzeit <plumbum.j@gmail.com>
2018-04-03 17:01:41 -0400

    Files using new temp file system

--- cryo/netcdf4gui.py
+++ cryo/netcdf4gui.py
@@ -1,21 +1,49 @@
-def readfrompipe(pipename):
-    #opens named pipe and reads parameters
-    with open(pipename, 'r') as fifo:
-        #changes format of parameters from
-        #'filename datamode readoutcard frames' (string) >>
-        #['filename', int(datamode), 'readoutcard', 'frames'] (list)
-        parameters = fifo.read().strip().split()
-        #converts parameters[1] (datamode) to integer (only to keep with rest of
-        #code, can be kept as string instead if needed)        
-        parameters[1] = int(parameters[1])
-    return parameters
+import time
+
+def readfromfiles(parafilename, heatfilename, graphfilename):
+    #opens all temp files
+    parafile = open(parafilename, 'r')
+    heatfile = open(heatfilename, 'r')
+    graphfile = open(graphfilename, 'r')
+    #reads parameters file and puts info into list
+    parameters = parafile.read().strip().split()
+
+    parameters[1] = int(parameters[1])
+    parafile.close()
+
+    #reads heatmap data file and puts data into nested list
+    #(in the same format as the z list)
+    heatmap = []
+    for line in heatfile:
+        x = line.strip().split()
+        for i in range(len(x)):
+            x[i] = int(x[i])
+        heatmap.append(x)
+    heatfile.close()
+
+    #reads graph data file and puts data into list
+    graph = graphfile.read().strip().split()
+    for i in range(len(graph)):
+        graph[i] = int(graph[i])
+    graphfile.close()
+
+    return parameters, heatmap, graph
 
 
 def main():
-    #pipename - whatever we decide to call pipe
-    pipename = 'gui2temp'
-    parameters = readfrompipe(pipename)
-    print(parameters)
+    while True:
+        #calls function with the built in temp file names
+        #outputs tuple of the 3 lists created by function
+        parameters, heatmap, graph = readfromfiles('tempparameters.txt', 'tempzdata.txt', 'tempgraphdata.txt')
+
+        #prints lists to check code is working correctly
+        print('Parameters:')
+        print(parameters)
+        print('Heatmap:')
+        print(heatmap)
+        print('Graph:')
+        print(graph)
+        time.sleep(0.1)
 
 if __name__ == '__main__':
     main()
