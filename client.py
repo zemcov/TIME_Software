3b543563fd218ddc399d5004bbee9076d586b38a
Victoria Butler (RIT Student) <vlb9398@cos-phy711.main.ad.rit.edu>
2018-06-28 15:08:26 -0400

    socket updates

--- client.py
+++ client.py
@@ -2,11 +2,12 @@ import socket, struct, threading
 
 PILOT1 = '129.21.172.16' #client
 PILOT1_PORT = 8888
-PILOT2 = '129.21.172.211' #server
-PILOT2_PORT = 500008
+MY_MAC = '129.21.61.159' #server
+MAC_PORT = 500008
+PILOT2 = '129.21.172.211'
 
 # I am the telescope sending data to the gui
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((PILOT2, PILOT2_PORT))
+s.connect((MY_MAC, MAC_PORT))
 s.send('Hello Scientist :)')
--- server.py
+++ server.py
@@ -4,19 +4,25 @@ PILOT1 = '129.21.172.16' #client
 PILOT1_PORT = 8888
 PILOT2 = '129.21.172.211' #server
 PILOT2_PORT = 500008
+MY_MAC = '129.21.61.159' #server
+MAC_PORT = 50008
 
 # I am accepting telescope sim data for the gui
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind('127.0.0.1',PILOT1_PORT)
-print('Server listening on localhost port 8888')
+s.bind(('127.0.0.1',MAC_PORT))
+print('Server listening on port %i' %(MAC_PORT))
 s.listen(5)
-unpacker = struct.Struct('f?ffffs')
+#unpacker = struct.Struct('f?ffffs')
+client, info = s.accept()
+unpacker = struct.Struct('5s')
 # f is float
 # ? is boolean
 while True:
-    data = s.recv(unpacker.size)
-    pa,slew_flag,alt,az,ra,dec,time = unpacker.unpack(data)
+    data = client.recv(unpacker.size)
+    #pa,slew_flag,alt,az,ra,dec,time = unpacker.unpack(data)
+    print('Data Received')
+    hello = unpacker.unpack(data)
 
     # (client,address) = s.accept()
     # ctrl_thread = threading.Thread(target=client)
