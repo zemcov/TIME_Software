fc3abdbc35931c6a988f6579eb7e0b89a2e53b3a
Victoria Butler <vlb9398@g.rit.edu>
2018-08-30 14:03:19 -0400

    deleted extra sftp files

--- main/mce1_cdm.sh
+++ main/mce1_cdm.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 #RC = $1
 #FRAME = $2
-ssh -T pilot2@timemce.rit.edu /usr/mce/bin/mce_cmd "-x wb rc$1 data_mode $2"
-
+ssh -T time@time-mce-0.caltech.edu /usr/mce/bin/mce_cmd "-x wb rc$1 data_mode $2"
--- main/mce1_cdr.sh
+++ main/mce1_cdr.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #DR = $1
-ssh -T pilot2@timemce.rit.edu /usr/mce/bin/mce_cmd "-x wb cc data_rate $1"
+# this is for the mce computer...
+ssh -T time@time-mce-0.caltech.edu /usr/mce/bin/mce_cmd "-x wb cc data_rate $1"
