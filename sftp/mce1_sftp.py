8bac8e24f9d33594ef9890c6103115ae47fee774
ausderzeit <plumbum.j@gmail.com>
2018-09-07 16:40:54 -0400

    better stop and separate sftp

--- sftp/mce1_sftp.py
+++ sftp/mce1_sftp.py
@@ -3,16 +3,18 @@ import subprocess
 import time
 import shutil
 import sys
+import datetime as dt
 
 def main(a, n_files):
     a = int(a)
     print('Hello!')
+    begintimer = dt.datetime.utcnow()
     while True:
         mce_file = os.path.exists("/data/cryo/current_data/temp.%0.3i" %(a+1)) #wait to read new file until old file is complete
         if mce_file:
             while mce_file:
                 #shutil.move('/data/cryo/current_data/temp.%0.3i' % (a), 'pilot1@time.rit.edu:/home/pilot1/ssh_stuff/mce1/temp.%0.3i' % (a))
-                print('sftp: %s' % (a))
+                #print('sftp: %s' % (a))
                 mce_file_name = '/data/cryo/current_data/temp.%0.3i' % (a)
                 if os.path.exists(mce_file_name):
                     if a == 1:
@@ -24,11 +26,15 @@ def main(a, n_files):
                     subprocess.Popen(delete_file,shell=True)
                     a += 1
                     mce_file = os.path.exists("/data/cryo/current_data/temp.%0.3i" %(a+1))
+                    begintimer = dt.datetime.utcnow()
                 else:
                     pass
-            sys.exit(a)
         else:
             pass
+        endtimer = dt.datetime.utcnow()
+        if endtimer - begintimer > dt.timedelta(seconds=5):
+            print('SFTP Stopped')
+            sys.exit()
 
 '''
 need some good way to stop this process from happening at the end of the gui
