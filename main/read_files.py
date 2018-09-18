8430449e348b4a3e526ca3b9d06ee913298a3d32
Victoria Butler <vlb9398@g.rit.edu>
2018-09-18 16:53:44 -0400

    logical error

--- main/read_files.py
+++ main/read_files.py
@@ -18,16 +18,8 @@ def netcdfdata(rc):
     filestarttime = 0
     mcea = subprocess.Popen(['ssh -T time@time-mce-1.caltech.edu python /home/time/time-software/sftp/mce1_sftp.py %s %s' % (a, n_files)], shell=True)
     while True:
-        #mce_file_name = "/data/cryo/current_data/temp.%0.3i" %(a)
-        #mce_file = os.path.exists("/data/cryo/current_data/temp.%0.3i" %(a+1)) #wait to read new file until old file is complete
-        #if mce_file:
-            #print('NETCDF IS WORKING')
-            #print(len(os.listdir("/data/cryo/current_data")) - 2 - n_files)
-
         mce_file = os.path.exists('/home/time/Desktop/time-data/mce1/temp.%0.3i' %(a+1))
-        #print('/home/time/Desktop/time-data/mce1/temp.%0.3i' %(a+1))
         if mce_file:
-            #print(len(os.listdir("/home/time/Desktop/time-data/mce1")) - 2)
             for i in range(len(os.listdir("/home/time/Desktop/time-data/mce1")) - 3):
                 print('netcdf: %s' % (a))
                 mce_file_name = '/home/time/Desktop/time-data/mce1/temp.%0.3i' %(a)
@@ -36,9 +28,8 @@ def netcdfdata(rc):
                 header = read_header(f)
                 mce, n, filestarttime = readdata(f, mce_file_name, mce, header, n, a, filestarttime, rc)
                 mce_file = os.path.exists('/home/time/Desktop/time-data/mce1/temp.%0.3i' %(a+1))
-            #else:
-            #    pass
-        if os.path.exists('tempfiles/stop.txt'):
+
+        elif os.path.exists('tempfiles/stop.txt'):
             subprocess.Popen(["ssh -T time@time-mce-1.caltech.edu /usr/mce/bin/mce_cmd '-x stop rcs ret_dat'"], shell=True)
             subprocess.Popen(['ssh -T time@time-mce-1.caltech.edu "rm /data/cryo/current_data/temp.*"'], shell=True)
             subprocess.Popen(['rm /home/time/Desktop/time-data/mce1/temp.*'], shell=True)
