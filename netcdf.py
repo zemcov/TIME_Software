5ddde4229bc52347f4aab362a6f1817d434da299
Victoria Butler <vlb9398@g.rit.edu>
2018-09-26 13:42:20 -0400

    i'm dum dum

--- netcdf.py
+++ netcdf.py
@@ -90,7 +90,7 @@ def data_all(h,n,head,filestarttime):
     Rms_Noise_All[n,:,:] = d
     pa,slew_flag,alt,az,ra,dec = np.loadtxt('tempfiles/tempteledata.txt',delimiter = ',', unpack=True)
     print(pa)
-    Tel[n,;,;] = [pa,slew_flag,alt,az,ra,dec]
+    Tel[n,:,:] = [pa,slew_flag,alt,az,ra,dec]
 
     #print Raw_Data_All.shape
     #new_head = np.array([head],dtype='S15').reshape((2,16))
@@ -105,7 +105,7 @@ def data(h,d,n,head,filestarttime):
     Rms_Noise[n,:,:] = d
     pa,slew_flag,alt,az,ra,dec = np.loadtxt('tempfiles/tempteledata.txt',delimiter = ',', unpack=True)
     print(pa)
-    Tel[n,;,;] = [pa,slew_flag,alt,az,ra,dec]
+    Tel[n,:,:] = [pa,slew_flag,alt,az,ra,dec]
     #new_head = np.array([head],dtype='S3')
     #print new_head
     #Header[a,:,:] = new_head
