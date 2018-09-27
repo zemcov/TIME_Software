5beaec8c93584b52257a74e67e31b5aaa9608ba6
Victoria Butler <vlb9398@g.rit.edu>
2018-09-27 17:20:56 -0400

    trying to get it to recognize s without a settings file

--- readteledata.py
+++ readteledata.py
@@ -1,20 +1,22 @@
 import socket, struct, subprocess
-import settings as st
 
 PORT = 8888
 # I am accepting tel socket packets as server
 run = True
 tele = []
-
+s = 0
 def main():
-    st.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    st.s.bind(('',PORT))
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('',PORT))
     print('Server listening on port %i' %(PORT))
-    st.s.listen(5)
+    s.listen(5)
 
     unpacker = struct.Struct('d d d d d d d')
-    client, info = st.s.accept()
+    client, info = s.accept()
+    loop()
+    return s
 
+def loop():
     while run == True:
         data = client.recv(unpacker.size)
         pa,slew_flag,alt,az,ra,dec,time = unpacker.unpack(data)
@@ -23,12 +25,12 @@ def main():
         f.write("\n%.06f,%.06f,%.06f,%.06f,%.06f,%.06f" %(pa, slew_flag, alt, az, ra, dec))
         f.close()
 
-def stop_sock(s):
-    run = False
-    print("Client Socket Shutdown")
-    s.shutdown(socket.SHUT_RDWR)
-    s.close()
-    sys.exit()
+# def stop_sock(s):
+#     run = False
+#     print("Client Socket Shutdown")
+#     s.shutdown(socket.SHUT_RDWR)
+#     s.close()
+#     sys.exit()
 
 
     #print('Data Received')
--- stop_server.py
+++ stop_server.py
@@ -1,8 +1,12 @@
 import sys
 sys.path.append('/home/pilot1/TIME_Software')
-from readteledata import stop_sock
-import settings as st
-import readteledata
-stop_sock(st.s)
+from readteledata import stop_sock,s
+stop_sock(s)
 sys.stdout.write('I also did a thing')
-sys.exit()
+
+def stop_sock(s):
+    run = False
+    print("Client Socket Shutdown")
+    s.shutdown(socket.SHUT_RDWR)
+    s.close()
+    sys.exit()
