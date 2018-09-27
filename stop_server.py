235cc9cea8937010eb2096fe28bef10e5f3b3078
Victoria Butler <vlb9398@g.rit.edu>
2018-09-27 16:48:28 -0400

    doesn't know which socket to stop

--- stop_server.py
+++ stop_server.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('/home/pilot1/TIME_Software')
-from readteledata import stop_sock
+from readteledata import stop_sock,s
 import readteledata
 stop_sock()
 sys.stdout.write('I also did a thing')
