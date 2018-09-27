81f0eb1951b3e2be6a47c33d3f880d1943d79b67
Victoria Butler <vlb9398@g.rit.edu>
2018-09-27 18:13:11 -0400

    I don't actually need a function

--- stop_server.py
+++ stop_server.py
@@ -1,15 +1,8 @@
 import sys
 sys.path.append('/home/pilot1/TIME_Software')
 from readteledata import s
-stop_sock(s)
-sys.stdout.write('I also did a thing')
 
-def stop_sock(s):
-    print("Client Socket Shutdown")
-    s.shutdown(socket.SHUT_RDWR)
-    s.close()
-    sys.exit()
-
-from readteledata import s
-stop_sock(s)
-sys.stdout.write('I also did a thing')
+print("Client Socket Shutdown")
+s.shutdown(socket.SHUT_RDWR)
+s.close()
+sys.exit()
