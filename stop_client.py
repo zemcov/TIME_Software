d5be8741b54ee6a5f49aa3eba940735e0f29a618
Victoria Butler <vlb9398@g.rit.edu>
2018-09-27 15:09:51 -0400

    doesn't look like sockets are stopping

--- stop_client.py
+++ stop_client.py
@@ -2,9 +2,6 @@ import sys
 sys.path.append('/home/pilot2/TIME_Software')
 import tel_sock
 
-def main():
-    tel_sock.stop_sock()
-    sys.stdout.write('I did a thing')
-
-if __name__==__main__:
-    main()
+tel_sock.stop_sock()
+sys.stdout.write('I did a thing')
+sys.exit()
--- stop_server.py
+++ stop_server.py
@@ -2,8 +2,6 @@ import sys
 sys.path.append('/home/pilot1/TIME_Software')
 import readteledata
 
-def main():
-    readteledata.stop_sock()
-    sys.stdout.write('I also did a thing')
-if __name__==__main__:
-    main()
+readteledata.stop_sock()
+sys.stdout.write('I also did a thing')
+sys.exit()
