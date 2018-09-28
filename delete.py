d475716f97eb9b880ec33cd2c1125f079a32bc8b
Victoria Butler <vlb9398@g.rit.edu>
2018-09-28 12:28:54 -0400

    something to get rid of extra files from the last run

---
+++ delete.py
@@ -0,0 +1,6 @@
+import subprocess, sys
+sys.path.append('/home/pilot1/TIME_Software')
+
+subprocess.Popen(['ssh -T pilot2@timemce.rit.edu rm /data/cryo/current_data/temp*'],shell=True)
+subprocess.Popen(['rm /home/pilot1/Desktop/time-data/mce1/temp*'],shell=True)
+subprocess.Popen(['rm /home/pilot1/Desktop/time-data/netcdffiles/mce*'],shell=True)
