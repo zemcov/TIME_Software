6c9377b415eb5fc1b0a6923f4a7b166295e35d3b
Victoria Butler <vlb9398@g.rit.edu>
2018-09-28 12:30:11 -0400

    imported wrong path?

--- delete.py
+++ delete.py
@@ -1,5 +1,5 @@
 import subprocess, sys
-sys.path.append('/home/pilot1/TIME_Software')
+sys.path.append('/home/pilot1/Desktop/time-data/')
 
 subprocess.Popen(['ssh -T pilot2@timemce.rit.edu rm /data/cryo/current_data/temp*'],shell=True)
 subprocess.Popen(['rm /home/pilot1/Desktop/time-data/mce1/temp*'],shell=True)
