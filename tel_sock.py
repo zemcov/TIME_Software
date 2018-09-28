5198e66c8a69f8e56886fa295d1981b8ef1bd248
Victoria Butler <vlb9398@g.rit.edu>
2018-09-28 15:17:40 -0400

    needed to define s outside of function

--- tel_sock.py
+++ tel_sock.py
@@ -26,11 +26,10 @@ dec = 20 # static
 loops_deg = 2 #number of loops per degrees = loops_deg
 COLOR = 'black'
 # -------------------------------------------------------------------------
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 def main():
     PILOT1_PORT = 8888
     PILOT1 = '129.21.172.16' #I'm sending the socket packets to server
-
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((PILOT1, PILOT1_PORT))
     #message = 'Hello!'
 
