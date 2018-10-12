bc9390b5aa959325dd810015b586afc54878eab9
Victoria Butler <vlb9398@g.rit.edu>
2018-10-12 17:58:38 -0400

    forgot to edit sftp files

--- sftp/mce1_sftp.py
+++ sftp/mce1_sftp.py
@@ -6,35 +6,30 @@ import sys
 import datetime as dt
 
 def main():
-    a = int(a)
-    print('=========== FILE TRANSFER STARTED =============')
-    begintimer = dt.datetime.utcnow()
-    while True:
-        mce_file = os.path.exists("/data/cryo/current_data/temp.%0.3i" %(a+1)) #wait to read new file until old file is complete
-        if mce_file:
-            while mce_file:
-                #shutil.move('/data/cryo/current_data/temp.%0.3i' % (a), 'pilot1@time.rit.edu:/home/pilot1/ssh_stuff/mce1/temp.%0.3i' % (a))
-                #print('sftp: %s' % (a))
-                mce_file_name = '/data/cryo/current_data/temp.%0.3i' % (a)
-                if os.path.exists(mce_file_name):
-                    if a == 1:
-                        subprocess.Popen(['scp', '/data/cryo/current_data/temp.run',  'time-master:/home/time/Desktop/time-data/mce1/temp.run']).wait()
-                        delete_file = ['rm %s' % ('/data/cryo/current_data/temp.run')]
-                        subprocess.Popen(delete_file,shell=True)
-                    subprocess.Popen(['scp', mce_file_name,  'time-master:/home/time/Desktop/time-data/mce2/temp.%0.3i' % (a)]).wait()
-                    delete_file = ['rm %s' % (mce_file_name)]
-                    subprocess.Popen(delete_file,shell=True)
-                    a += 1
-                    mce_file = os.path.exists("/data/cryo/current_data/temp.%0.3i" %(a+1))
-                    begintimer = dt.datetime.utcnow()
-                else:
-                    pass
-        else:
-            pass
-        endtimer = dt.datetime.utcnow()
-        if endtimer - begintimer > dt.timedelta(seconds=5):
-            print 'SFTP1 Stopped'
-            sys.exit()
+    a = 0
+    print '----- Starting MCE1 Data Transfer -----'
+    begin = dt.datetime.utcnow()
+    end = dt.datetime.utcnow()
+    while end - begin < dt.timedelta(seconds = 3):
+        if os.path.exists("/data/cryo/current_data/temp.%0.3i" %(a+1)) #wait to read new file until old file is complete
+            mce_file_name = '/data/cryo/current_data/temp.%0.3i' % (a)
+            if a == 0:
+                subprocess.Popen(['scp', '/data/cryo/current_data/temp.run',  'time-master:/home/time/Desktop/time-data/mce2/temp.run']).wait()
+                subprocess.Popen(['rm %s' % ('/data/cryo/current_data/temp.run')],shell=True)
+            if os.path.exists(mce_file_name)
+            subprocess.Popen(['scp', mce_file_name,  'time-master:/home/time/Desktop/time-data/mce2/temp.%0.3i' % (a)]).wait()
+            delete_file = ['rm %s' % (mce_file_name)]
+            a += 1
+            begin = dt.datetime.utcnow()
+            else:
+                print "File Doesn't Exist!"
+        end = dt.datetime.utcnow()
+
+    else :
+        print 'SFTP0 Stopped'
+        subprocess.Popen(['rm /data/cryo/current_data/temp*'],shell=True)
+        sys.exit()
+
 
 if __name__ == '__main__':
     main()
