1ad7fc7c03b7fbeb7fc140229969161924ba7555
Victoria Butler <vlb9398@g.rit.edu>
2018-11-13 11:11:46 -0500

    gzip was reading in a list

--- main/read_files.py
+++ main/read_files.py
@@ -142,7 +142,7 @@ def hk_read(hk):
     # telling netcdf how many files worth of hk data to expect
     hk_files = len(hk)
     for i in range(len(hk)):
-        file = gzip.open(hk)
+        file = gzip.open(hk[i])
         for line in file:
             #fields = line.strip().split(',')
             a,b,c,d,e = line.strip().split(',')
