c3e24e40cc1cb8bd8e1420322a753f3cf9e7c66d
Victoria Butler <vlb9398@g.rit.edu>
2018-12-03 11:34:00 -0500

    whoops again

--- sftp/hk_sftp.py
+++ sftp/hk_sftp.py
@@ -13,22 +13,19 @@ a = 0
 dir = '/data/hk/omnilog/'
 print colored('Starting HK File Transfer','green')
 
-def main():
-    while True:
-        if a == 0 :
-            files = [dir + x for x in os.listdir(dir) if (x.startswith("omnilog") and x.endswith('.gz'))]
-            hk_file = max(files, key = os.path.getctime)
-            a = int(hk_file.replace(dir,'').replace('omnilog.','').replace('.txt.gz',''))
-            print colored('HK starting file = %i' %(a),'green')
-        else :
-            if os.path.exists(dir + "omnilog.%i.txt.gz" %(a+1)) : #wait to read new file until old file is complete
-                file_name = dir + 'omnilog.%i.txt.gz' % (a)
-                if os.path.exists(file_name) :
-                    subprocess.Popen(['scp', file_name,  'time@time-master.caltech.edu:/home/time/Desktop/time-data/hk/omnilog.%i.txt.gz' % (a)]).wait()
-                    print colored('HK File Transfered : %s' %(file_name),'magenta')
-                    a = a + 1
-                else:
-                    print "HK File Doesn't Exist!"
+while True:
+    if a == 0 :
+        files = [dir + x for x in os.listdir(dir) if (x.startswith("omnilog") and x.endswith('.gz'))]
+        hk_file = max(files, key = os.path.getctime)
+        a = int(hk_file.replace(dir,'').replace('omnilog.','').replace('.txt.gz',''))
+        print colored('HK starting file = %i' %(a),'green')
+    else :
+        if os.path.exists(dir + "omnilog.%i.txt.gz" %(a+1)) : #wait to read new file until old file is complete
+            file_name = dir + 'omnilog.%i.txt.gz' % (a)
+            if os.path.exists(file_name) :
+                subprocess.Popen(['scp', file_name,  'time@time-master.caltech.edu:/home/time/Desktop/time-data/hk/omnilog.%i.txt.gz' % (a)]).wait()
+                print colored('HK File Transfered : %s' %(file_name),'magenta')
+                a = a + 1
+            else:
+                print "HK File Doesn't Exist!"
                 
-if __name__ == '__main__':
-    main()
