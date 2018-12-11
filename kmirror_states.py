1c9469356df8a96033e4f078246ccdbe8bbf6f6a
Victoria Butler <vlb9398@g.rit.edu>
2018-12-11 16:08:00 -0500

    adding back in socket for gui

--- kmirror_states.py
+++ kmirror_states.py
@@ -352,9 +352,9 @@ class Stop_Checker():
                     pa,flag = unpacker.unpack(data)
                     update = TelescopeUpdate(pa_enc(float(pa)), time.time(), time.time(), flag)
                     self.masterlist.append(update)
-                    pa_file.writelines([str(pa) + ' , ',str((get_pos() - home_pos)/2.0) + '\n'])
-                    # self.pa = pa
-                    # self.slew_flag = flag
+                    # pa_file.writelines([str(pa) + ' , ',str((get_pos() - home_pos)/2.0) + '\n'])
+                    self.pa = pa
+                    self.slew_flag = flag
             except KeyboardInterrupt:
                 if connection :
                     connection.close()
@@ -381,20 +381,20 @@ class Stop_Checker():
                 break
         self.thread1Stop.set()
 #########################################################################
-    # def vic_socket(self):
-    #     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #     s.connect((CONTROL_HOST, CONTROL_PORT))
-    #     packer = packer = struct.Struct('d d d i')
-    #     while not self.thread1Stop.is_set():
-    #         try:
-    #             data = packer.pack(float(self.pa.value),float(pa_enc(get_pos())),float(time.time()),int(self.slew_flag.value))
-    #             s.send(data)
-    #             time.sleep(0.05)
-    #             print 'PA Sent to Gui',time.time()
-    #         except KeyboardInterrupt:
-    #             self.thread1stop.set()
-    #     s.close()
-    #     print 'vic_socket closed'
+    def vic_socket(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((CONTROL_HOST, CONTROL_PORT))
+        packer = packer = struct.Struct('d d d i')
+        while not self.thread1Stop.is_set():
+            try:
+                data = packer.pack(float(self.pa.value),float(pa_enc(get_pos())),float(time.time()),int(self.slew_flag.value))
+                s.send(data)
+                time.sleep(0.05)
+                print 'PA Sent to Gui',time.time()
+            except KeyboardInterrupt:
+                self.thread1stop.set()
+        s.close()
+        print 'vic_socket closed'
 
 ###############################################################################################################################
     def main(self,arg1,arg2,arg3):
@@ -415,11 +415,11 @@ class Stop_Checker():
             t1 = mp.Process(target=self.limits,args=(arg3,))
             t2 = mp.Process(target=self.run)
             t3 = mp.Process(target=self.track)
-            # t4 = mp.Process(target=self.vic_socket)
+            t4 = mp.Process(target=self.vic_socket)
             t1.start()
             t2.start()
             t3.start()
-            # t4.start()
+            t4.start()
         self.stop_check()
 
 # ====================================================================================================
