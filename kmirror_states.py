b84abb5e1e20c19822e4b0033f026d5f893cd297
Victoria Butler <vlb9398@g.rit.edu>
2018-12-12 12:17:08 -0500

    reverting back to working kmirror_states.py

--- kmirror_states.py
+++ kmirror_states.py
@@ -12,7 +12,7 @@ https://dev.to/karn/building-a-simple-state-machine-in-python
 from state import *
 import RPi.GPIO as GPIO
 from safety import *
-from encoder import get_pos, home_pos, minimum, maximum, pa_enc, enc_pa
+from encoder import get_pos, home_pos, minimum, maximum, pa_enc
 from util import *
 from rotate_motor import rotate_motor, twos_comp
 import sys, subprocess
@@ -21,14 +21,11 @@ import numpy as np
 import PID
 import multiprocessing as mp
 from multiprocessing import Manager
-import matplotlib
-matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
 
 TELESCOPE_HOST = '0.0.0.0'
 TELESCOPE_PORT = 8888
-CONTROL_HOST = '192.168.0.101'
-CONTROL_PORT = 8500
+CONTROL_HOST = '192.168.0.102'
+CONTROL_PORT = 8000
 
 NUM_SAMPLES = 800
 STEP_OFFSET = 150
@@ -205,22 +202,14 @@ class Stop_Checker():
         kalman_filter = KalmanFilter(process_variance, estimated_measurement_variance)
         kmirror = KMirror()
 
-        debug_vars = {
-        'abs_update_degs_list': [],
-        'abs_encoder_degs_list': [],
-        'abs_err_arcsec_list': [],
-        'speed_list': [],
-        'motor_set_point': [],
-        }
-
         try:
+            # ===================================== TRACKING ALGORITHM MAIN LOOP =====================================
             old_len = len(self.masterlist)
             while not self.thread1Stop.is_set():
-            # ===================================== TRACKING ALGORITHM MAIN LOOP =====================================
                 # Wait for a new update to come in
                 while len(self.masterlist) <= old_len:
                     time.sleep(0.02)
-		        #print("updating...")
+		print("updating...")
                 old_len = len(self.masterlist)
 
                 # NEW UPDATE
@@ -231,8 +220,7 @@ class Stop_Checker():
                     tele_vel = self.masterlist[-1].abs_degree_pos - self.masterlist[-2].abs_degree_pos
                     time_delta = self.masterlist[-1].when_sent - self.masterlist[-2].when_sent
                     if time_delta == 0.0 :
-		    	        tele_vel /= 0.1
-
+		    	tele_vel /= 0.1
                     if (tele_vel > 0 and measured_speed[-1] < 0) or (tele_vel < 0 and measured_speed[-1] > 0) or \
                         self.masterlist[-1].flag != self.masterlist[-2].flag:
                         kalman_filter.reset(tele_vel)
@@ -242,81 +230,17 @@ class Stop_Checker():
 
                     if len(self.masterlist) >= 3:
                         kmirror.update()
-                        ''' debug stuff '''
-                        self.update_debugs(debug_vars, kmirror, self.masterlist[-1])
                     encoder_pos_d = step_to_deg(kmirror.encoder_pos_s)
                     set_point = deg_to_step(ang_subtract(self.masterlist[-1].abs_degree_pos, encoder_pos_d))
                     set_point *= ang_subtract_sign(self.masterlist[-1].abs_degree_pos, encoder_pos_d)
                     steps_to_move = int(set_point) + STEP_OFFSET * sign(kmirror.speed)
                     kmirror.move_cmd(steps_to_move, set_point, kalman_speed[-1])
-                ''' debug stuff '''
-                if len(debug_vars['speed_list']) >= NUM_SAMPLES:
-                    self.thread1Stop.set()
-        except KeyboardInterrupt():
-            self.thread1Stop.set()
-            print("Tracking has stopped")
-            # ====================================== OPTIONAL CODE FOR DEBUGGING ======================================
-            errors = np.array(debug_vars['abs_err_arcsec_list'])
-            errors = np.absolute(errors)
-            print "Average error (arcsec): {:3.3f}".format(np.mean(errors))
-
-            x_axis = range(len(debug_vars['speed_list']))
-            packet_lag = [(update.when_received - update.when_sent)*1000.0 for update in self.masterlist[:len(x_axis)]]
-            print "Average Packet Lag (ms): {:3.3f}".format(np.mean(np.absolute(np.array(packet_lag))))
-            print "Std Packet Lag (ms): {:3.3f}".format(np.std(np.absolute(np.array(packet_lag))))
-
-            # attempt to clean encoder signal
-            avg_encoder = np.mean(np.absolute(np.array(debug_vars['abs_encoder_degs_list'])))
-            encoder_signal = [val if abs(val) < avg_encoder * 10 else avg_encoder for val in debug_vars['abs_encoder_degs_list']]
-            print "Deleted %s outlier values" % np.sum(np.array([0 if abs(val) < avg_encoder * 10 else 1 for val in debug_vars['abs_encoder_degs_list']]))
-
-            # # graph debug stuff
-            # f, axarr = plt.subplots(3, sharex=True)
-            #
-            # line, = axarr[0].plot(x_axis, encoder_signal)
-            # line, = axarr[0].plot(x_axis, debug_vars['abs_update_degs_list'])
-            # axarr[0].set(ylabel='degrees')
-            # axarr[0].set_title('Absolute Rotational Position')
-            #
-            # line, = axarr[1].plot(x_axis, debug_vars['abs_err_arcsec_list'])
-            # line, = axarr[1].plot(np.zeros(len(x_axis)))
-            # axarr[1].set(ylabel='arcsec')
-            # axarr[1].set_title('Error')
-            #
-            # line, = axarr[2].plot(x_axis, debug_vars['speed_list'])
-            # line, = axarr[2].plot(np.zeros(len(x_axis)))
-            # axarr[2].set(ylabel='steps/sec')
-            # axarr[2].set_title('Motor Speed')
-            #
-            # f2, axarr2 = plt.subplots(2, sharex=True)
-            # plt.subplots_adjust(left=.04, right=.95, bottom=.04, top=.95, wspace=.20, hspace=.20)
-            #
-            # del measured_speed[:2]
-            # del kalman_speed[0]
-            # if len(measured_speed) > len(x_axis):
-            #     axarr2[0].plot(x_axis, measured_speed[:len(x_axis)])
-            # else:
-            #     axarr2[0].plot(x_axis[:len(measured_speed)], measured_speed)
-            # axarr2[0].plot(x_axis, kalman_speed[:len(x_axis)])
-            # axarr2[0].set(ylabel='degrees/sec')
-            # axarr2[0].set_title('Kalman Response to Velocity')
-            #
-            # axarr2[1].plot(x_axis, packet_lag)
-            # axarr2[1].plot(np.zeros(len(x_axis)))
-            # axarr2[1].set(ylabel='ms')
-            # axarr2[1].set_title('Packet lag')
-            # plt.show()
 
             # ========================================================================================================
-    def update_debugs(self,debug_vars, kmirror, last_update):
-        error_s = deg_to_step(last_update.abs_degree_pos) - kmirror.encoder_pos_s
-        error_arcsec = deg_to_arcsec(step_to_deg(error_s))
-        #print "Error (steps, arcsec): {:3.3f}, {:3.3f}".format(error_s, error_arcsec)
-        debug_vars['abs_update_degs_list'].append(last_update.abs_degree_pos)
-        debug_vars['abs_encoder_degs_list'].append(step_to_deg(kmirror.encoder_pos_s))
-        debug_vars['speed_list'].append(kmirror.speed)
-        debug_vars['abs_err_arcsec_list'].append(error_arcsec)
-        debug_vars['motor_set_point'].append(step_to_deg(kmirror.dest_pos))
+        except KeyboardInterrupt:
+            self.thread1Stop.set()
+        finally:
+            print("Tracking has stopped")
 ##################################################################################################################################
     def run(self):
         print("run is starting")
@@ -339,28 +263,23 @@ class Stop_Checker():
         print('listening for connection')
         unpacker = struct.Struct('d i')
         while not self.thread1Stop.is_set():
-            pa_file = open('pa.txt','w')
-            pa_file.close()
-            pa_file = open('pa.txt', 'a')
-            connection = None
             connection,client= s.accept()
             print('Socket connected')
             try:
                 while not self.thread1Stop.is_set():
-                    data = connection.recv(unpacker.size)
-                    pa,flag = unpacker.unpack(data)
-                    update = TelescopeUpdate(pa_enc(float(pa)), time.time(), time.time(), flag)
-                    self.masterlist.append(update)
-                    pa_file.writelines([str(pa) + ' , ',str(enc_pa(get_pos())) + '\n'])
-                    # self.pa = pa
-                    # self.slew_flag = flag
-            except KeyboardInterrupt:
-                if connection :
+                   data = connection.recv(unpacker.size)
+                   if data :
+                       pa,flag = unpacker.unpack(data)
+                       update = TelescopeUpdate(pa_enc(float(pa)), time.time(), time.time(), flag)
+##                       print(pa,flag,pa_enc(float(pa)))
+                       self.masterlist.append(update)
+                   else : #no more data
+                       break
+            except Exception as e:
+                print e
+                s.close()
+            finally :
                     connection.close()
-                pa_file.close()
-                self.thread1Stop.set()
-
-        s.close()
 ################################################################################################################################
     def step_overload(self,num_steps) :
         steps = num_steps
@@ -380,27 +299,20 @@ class Stop_Checker():
                 break
         self.thread1Stop.set()
 #########################################################################
-    # def vic_socket(self):
-    #     while not self.thread1Stop.is_set():
-    #         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #         s.connect((CONTROL_HOST, CONTROL_PORT))
-    #         packer = packer = struct.Struct('d d d i')
-    #         try:
-    #             while not self.thread1Stop.is_set():
-    #                 data = packer.pack(float(self.pa.value),float(pa_enc(get_pos())),float(time.time()),int(self.slew_flag.value))
-    #                 s.send(data)
-    #                 time.sleep(0.05)
-    #                 print 'PA Sent to Gui',time.time()
-    #         except KeyboardInterrupt:
-    #             self.thread1stop.set()
-    #     s.close()
-    #     print 'vic_socket closed'
+#    def vic_socket():
+#        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#        s.connect((CONTROL_HOST, CONTROL_PORT))
+#        packer = packer = struct.Struct('d i')
+#	while not self.thread1Stop.is_set():
+#		data = packer.pack(float(pa),int(slew_flag))
+#    		s.send(data)
+#        	print 'PA Sent to Gui',time.time()
+#	s.close()
+#	print 'vic_socket closed'
 
 ###############################################################################################################################
     def main(self,arg1,arg2,arg3):
         self.masterlist = Manager().list()
-        # self.pa = Manager().Value('d',0.0)
-        # self.slew_flag = Manager().Value('i',0)
         if arg1 == 'go_to':
             t1 = mp.Process(target=self.go_to,args = (arg2,))
             t2 = mp.Process(target=self.limits,args = (arg3,))
@@ -415,12 +327,13 @@ class Stop_Checker():
             t1 = mp.Process(target=self.limits,args=(arg3,))
             t2 = mp.Process(target=self.run)
             t3 = mp.Process(target=self.track)
-            # t4 = mp.Process(target=self.vic_socket)
+ #           t4 = mp.Process(target=self.vic_socket)
             t1.start()
             t2.start()
             t3.start()
-            # t4.start()
+#            t4.start()
         self.stop_check()
+
 
 # ====================================================================================================
 # Start of our states.
