b803198dfc66a0d993204af7a42e16048f0c6a7d
Victoria Butler <vlb9398@g.rit.edu>
2019-01-11 18:55:30 -0500

    THE TRIMMED_GUI IS WORKING!!!

--- main/trimmed_gui.py
+++ main/trimmed_gui.py
@@ -13,23 +13,45 @@ class ExampleApp(QtGui.QMainWindow, ui_main.Ui_MainWindow):
         self.setupUi(self)
         self.btnAdd.clicked.connect(self.update)
         self.grPlot.plotItem.showGrid(True, True, 0.7)
+        # these will init the thread and allow it to send messages
+        self.update()
 
     def update(self):
-        t1=time.clock()
-        points=100 #number of data points
-        X=np.arange(points)
-        Y=np.sin(np.arange(points)/points*3*np.pi+time.time())
+        # connecting thread functions
+        self.updater = MyThread()
+        self.updater.new_data.connect(self.data_update)
+        self.updater.start()
+
+    def data_update(self, t1, X, Y):
         C=pyqtgraph.hsvColor(time.time()/5%1,alpha=.5)
         pen=pyqtgraph.mkPen(color=C,width=10)
         self.grPlot.plot(X,Y,pen=pen,clear=True)
         print("update took %.02f ms"%((time.clock()-t1)*1000))
-        if self.chkMore.isChecked():
-            QtCore.QTimer.singleShot(1, self.update) # QUICKLY repeat
+
+class MyThread(QtCore.QThread):
+
+    new_data = QtCore.pyqtSignal(object,object,object)
+
+    def __init__(self, parent = None):
+        QtCore.QThread.__init__(self, parent)
+        self.exiting = False
+
+    def __del__(self):
+        self.exiting = True
+        self.wait()
+
+    def run(self):
+        while not self.exiting :
+            t1=time.clock()
+            points=100 #number of data points
+            X=np.arange(points)
+            Y=np.sin(np.arange(points)/points*3*np.pi+time.time())
+            self.new_data.emit(t1,X,Y)
+            time.sleep(1.0) # dunno if I need this or not
 
 if __name__=="__main__":
     app = QtGui.QApplication(sys.argv)
     form = ExampleApp()
     form.show()
-    form.update() #start with something
     app.exec_()
     print("DONE")
