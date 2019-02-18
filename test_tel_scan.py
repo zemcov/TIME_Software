0cd90f9a2c9eb86a39687828e214039078eddc10
Victoria Butler <vlb9398@g.rit.edu>
2019-02-18 12:54:25 -0700

    test tel comm updates

--- test_tel_scan.py
+++ test_tel_scan.py
@@ -3,7 +3,6 @@ import socket, struct, subprocess, os, sys
 import time
 import numpy as np
 import multiprocessing as mp
-tel_exit = mp.Event()
 
 class TIME_TELE :
 
@@ -14,17 +13,24 @@ class TIME_TELE :
         self.s.connect((HOST,PORT))
         print('TCOMM Socket Connected')
         ack = struct.Struct('s')
-        message = tel_init(sec,map_size,map_angle,coord,epoch,object)
-        return message
+        # ======================================================================
+        cmnd_list = 'TIME_START_TELEMETRY on'
+        self.s.send(cmnd_list.encode('utf-8'))
+        reply = self.s.recv(ack.size)
+            if 'done' in reply : # wait for ack from tel
+                print('TELESCOPE INITIALIZED, STATUS: READY')
+                self.start_sock_tracker()
+        # ======================================================================
 
-    def start_sock_tracker(self): # this is just receiving firehose of packets
+    def start_sock_tracker(self): # this is just receiving firehose of data packets
         PORT2 = 1825
-        HOST = '192.168.1.252'
+        HOST2 = '192.168.1.252'
         self.s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s2.connect((HOST,PORT))
+        self.s2.connect((HOST2,PORT2))
         print('Tracker Socket Connected')
         unpacker = struct.Struct('s i i i i d d d d d d d d d d d d d d d d') # d = float , s = char string , i = integer
 
+        # ===========================================================================================
         while True:
 
             if tel_exit.is_set(): # if shutdown command from software, send shutdown command to tel
@@ -36,7 +42,6 @@ class TIME_TELE :
                     break
 
             else :
-
                 data = self.s2.recv(unpacker.size)
                 # unpacking data packet ===============================================
                 name, blanking, direction, observing, pad, \
@@ -50,23 +55,8 @@ class TIME_TELE :
         self.s2.close()
         sys.exit()
 
-    def tel_init(self,sec,map_size,map_angle,coord):
-        i = 0
-        cmnd_list = ['TIME_START_TELEMETRY on','TIME_START_TRACKING off','TIME_SCAN_TIME %s','TIME_MAP_SIZE %s','TIME_MAP_ANGLE %s',\
-                        'TIME_MAP_COORD RA','SEEK %s %s %s %s' %(sec,map_size,map_angle,coord[0],coord[1],epoch,object)]
-        while i <= len(cmnd_list) :
-            self.s.send(cmnd_list[i].encode('utf-8'))
-            reply = self.s.recv(ack.size)
-            if 'done' in reply : # wait for ack from tel
-                if i == 0 :
-                    self.start_sock_tracker()
-                i += 1
-
-        message = 'TELESCOPE INITIALIZED, STATUS: READY'
-        return message
-
-
 if __name__ == '__main__':
-    Time_Tele().start_sock()
+    tel_exit = mp.Event()
+    TIME_TELE().start_sock_tcomm()
     time.sleep(10.0)
     tel_exit.set()
