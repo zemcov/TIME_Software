2071fda4f3e722673e6554e37083b66affbac374
Victoria Butler <vlb9398@g.rit.edu>
2019-02-18 19:43:39 -0700

    shutdown sequence for socket

--- tom_tel.py
+++ tom_tel.py
@@ -12,12 +12,24 @@ def start_sock_tracker():
     client, info = s.accept()
     print('Socket Connected')
     unpacker = struct.Struct('i i i i d d d d d d d d d d d d d d d d') # d = float , s = char string , i = integer
-
+    i = 0
     while True :
         data = client.recv(unpacker.size)
-        # print('package size',unpacker.size)
-        # print('Data Received')
-        # unpacking data packet ===============================================
-        blanking, direction, observing, pad, ut, lst, deltaT, cur_ra, cur_dec, map_ra, map_dec, ra_off, dec_off, az, el, azvelcmd, elvelcmd, azvelact, elvelact, pa = unpacker.unpack(data)
-        # print('ut:',ut)
-        # ==================================================================
+        if i < 3 :
+            if len(data) !=0 :
+                # print('package size',unpacker.size)
+                # print('Data Received')
+                # unpacking data packet ===============================================
+                blanking, direction, observing, pad, ut, lst, deltaT, cur_ra, cur_dec,\
+                map_ra, map_dec, ra_off, dec_off, az, el, azvelcmd, elvelcmd, azvelact,\
+                elvelact, pa = unpacker.unpack(data)
+                # print('ut:',ut)
+                # ==================================================================
+            else :
+                print('waiting for data')
+                i += 1
+        else :
+            break
+            
+    s.close()
+    sys.exit()
