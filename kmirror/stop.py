63d46a998100eb2e105674afdb407b5daa1bf075
Evan Mayer <evanmayer@uchicago.edu>
2021-10-19 13:36:26 -0700

    remove kmirror dir, as it has been relocated to it's own repo

--- kmirror/stop.py
+++
@@ -1,8 +0,0 @@
-import RPi.GPIO as GPIO
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup([20,21],GPIO.OUT)
-GPIO.output(21,GPIO.HIGH)
-GPIO.output(20,GPIO.HIGH)
-print("KMIRROR STOPPED")
